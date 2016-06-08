ca712536420c46d65f0396c029a448b3879ecaba
Sathish Kumar <iamsathi95@gmail.com>
2016-06-08 20:57:11 +0530

    Update guvi_hunt_sub_array_with_max_sum.py

--- guvi_hunt_sub_array_with_max_sum.py
+++ guvi_hunt_sub_array_with_max_sum.py
@@ -2,7 +2,7 @@ L = map(int,raw_input().split())
 P = []
 maxi = -99999
 for i in range(0,len(L)):
-    for j in range(i+1,len(L)):
+    for j in range(i+1,len(L)+1):
         if sum(L[i:j]) > maxi :
             maxi = sum(L[i:j])
             P = L[i:j]
